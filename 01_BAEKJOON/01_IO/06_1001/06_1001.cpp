614bef1ba830bc492139923655bb1a410a4ee3d1
DESKTOP-JGF2CLD\ZeroB <ook223@gmail.com>
2022-06-12 09:50:23 +0900

    # 0611 풀이

---
+++ 01_BAEKJOON/01_IO/06_1001/06_1001.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a(0);
+    int b(0);
+    cin >> a >> b;
+
+    if (a <= 0 || b > 10)
+    {
+        cout << "숫자의 범위를 확인하세요" << endl;
+        return 0;
+    }
+
+    cout << a - b << endl;
+
+    return 0;
+}
---
+++ 01_BAEKJOON/01_IO/08_1008/08_1008.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a(0);
+    int b(0);
+    double out(0);
+
+    cin >> a >> b;
+
+    if (a <= 0 || b > 10)
+    {
+        cout << "숫자의 범위를 확인하세요" << endl;
+        return 0;
+    }
+    out = a / b;
+
+    out = double(a) / double(b);
+    
+    cout << fixed;
+    cout.precision(12);
+    cout << out << endl;
+
+    return 0;
+}
+
+// fixed, precision 함수 조사
+// double 소숫점 신뢰 범위 확인 : 약 14자리
+//

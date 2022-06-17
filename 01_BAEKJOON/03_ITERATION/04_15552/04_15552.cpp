2c2e1937627008ae51dc73a74ea3ff09f5fdf7a0
DESKTOP-JGF2CLD\ZeroB <ook223@gmail.com>
2022-06-17 09:16:47 +0900

    #  0616 풀이

--- 01_BAEKJOON/03_ITERATION/04_15552/04_15552.cpp
+++ 01_BAEKJOON/03_ITERATION/04_15552/04_15552.cpp
@@ -4,37 +4,34 @@ using namespace std;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int nNumCase(0);
-    cin >> nNumCase;
+	int nNumCase(0);
+	cin >> nNumCase;
 
-    if ((nNumCase > 1000000) || (nNumCase < 0))
-    {
-        cout << "정수의 범위를 확인하세요." << "\n";
-        return 0;
-    }
-
-
-    int nNum1(0), nNum2(0);
+	if ((nNumCase > 1000000) || (nNumCase < 0))
+	{
+		cout << "정수의 범위를 확인하세요." << "\n";
+		return 0;
+	}
 
 
-    for (int i = 0; i < nNumCase; i++)
-    {
-        cin >> nNum1 >> nNum2;
+	int nNum1(0), nNum2(0);
 
-        if ((nNum1 > 1000) || (nNum1 < 0) || (nNum2 > 1000) || (nNum2 < 0))
-        {
-            cout << "정수의 범위를 확인하세요." << "\n";
-            return 0;
-        }
-        cout << nNum1 + nNum2 << "\n";
-    }
 
-    return 0;
+	for (int i = 0; i < nNumCase; i++)
+	{
+		cin >> nNum1 >> nNum2;
+
+		if ((nNum1 > 1000) || (nNum1 < 0) || (nNum2 > 1000) || (nNum2 < 0))
+		{
+			cout << "정수의 범위를 확인하세요." << "\n";
+			return 0;
+		}
+		cout << nNum1 + nNum2 << "\n";
+	}
+
+	return 0;
 }
-
-// 틀린문제
-// 입출력에 관한 속도를 묻는 문제로 크게 사용성이 있어보이지는 않음
---
+++ 01_BAEKJOON/03_ITERATION/14_1110/14_1110.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int nNum(0), nNumRef(0),nNumNew(0), nCnt(0);
+	int nFront(0), nBack(0);
+
+	bool flag_ext = false;
+	cin >> nNum;
+
+	if (nNum > 99 || nNum < 0)
+	{
+		cout << "값을 확인하세요" << '\n';
+		return 0;
+
+	}
+
+	nNumRef = nNum;
+	while (!flag_ext)
+	{
+		if (nNum < 10)
+		{
+			nNum * 10;
+		}
+		nFront = nNum/10;
+		nBack = nNum % 10;
+		nNumNew = nFront + nBack;
+		nNum = (nNum % 10) * 10 + (nNumNew % 10);
+
+		if (nNum == nNumRef)
+			flag_ext = true;
+
+		nCnt++;
+	}
+	cout << nCnt << '\n';
+
+	return 0;
+}

a8f9b8bc55f6e70780550b7745eede3b4062790d
DESKTOP-JGF2CLD\ZeroB <ook223@gmail.com>
2022-06-17 09:04:17 +0900

    #0616 풀이

---
+++ 01_BAEKJOON/03_ITERATION/05_2741/05_2741.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nNum(0);
+    cin >> nNum;
+
+    if ((nNum > 100000) || (nNum < 0))
+    {
+        cout << "자연수의 범위를 확인하세요." << '\n';
+        return 0;
+    }
+
+    for (int i = 0; i < nNum; i++)
+    {
+        cout << i+1 << '\n';
+    }
+
+    return 0;
+}
---
+++ 01_BAEKJOON/03_ITERATION/07_11021/07_11021.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nCase(0);
+	cin >> nCase;
+
+	int* nNum1 = new int[nCase];
+	int* nNum2 = new int[nCase];
+
+	for (int i = 0; i < nCase; i++)
+	{
+		cin >> nNum1[i] >> nNum2[i];
+
+		if (((nNum1[i] > 10) || (nNum1[i] < 0)) || ((nNum2[i] > 10) || (nNum2[i] < 0)))
+		{
+			cout << "자연수의 범위를 확인하세요." << '\n';
+			return 0;
+		}
+
+	}
+
+	for (int i = 0; i < nCase; i++)
+	{
+		cout << "Case #" << i+1 << ": " << nNum1[i] + nNum2[i] << '\n';
+	}
+
+	delete[] nNum1;
+	delete[] nNum2;
+
+	return 0;
+}

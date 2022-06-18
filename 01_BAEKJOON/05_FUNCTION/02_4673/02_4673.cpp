f223e64814bb09ab5b5b6b15efc709b33d32abd1
DESKTOP-JGF2CLD\ZeroB <ook223@gmail.com>
2022-06-18 23:53:40 +0900

    # 0618 풀이

---
+++ 01_BAEKJOON/05_FUNCTION/02_4673/02_4673.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int series_Kaprekar(int number);
+
+int main()
+{
+    const int   MAX_NUM = 10000;
+    int         nNum_Kaprekar(0);
+    bool        nNum_self[MAX_NUM+1] = { false, };
+
+
+
+    for (int i = 1; i < MAX_NUM+1; i++)
+    {
+        if (series_Kaprekar(i) < MAX_NUM+1)
+        {
+            nNum_self[series_Kaprekar(i)] = true;
+        }
+        else
+        {
+            continue;
+        }
+
+    }
+
+    for (int i = 1; i < MAX_NUM+1; i++)
+    {
+        if (nNum_self[i] == false)
+        {
+            cout << i << '\n';
+        }
+    }
+
+
+
+
+    return 0;
+}
+
+
+int series_Kaprekar(int number)
+{
+    int num_iteration(0);
+    int num(0);
+    vector<string> v;
+
+    string szNumber;
+    string szNumber_single;
+
+    szNumber = to_string(number);
+ 
+    num_iteration = szNumber.size();
+ 
+    for (int i = 0; i < num_iteration; i++)
+    {
+        szNumber_single = szNumber.at(i);
+        v.push_back(szNumber_single);
+
+        num += stoi(v.at(i));
+    }
+
+    return num + number;
+}
